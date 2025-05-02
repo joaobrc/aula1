6fe64164a5ea275af509117bd59e752ef6a4567e
Joao <joaobrc97@gmail.com>
2025-05-02 13:04:14 -0300

    feat: adcionando um exercio de nomes

---
+++ src/aula1/exercicio_1.py
@@ -0,0 +1,4 @@
+# Criar programa o usuario digite seu nome e retorne a quantidade de caractes do nome
+nome = input('Digite seu nome: ')
+quant_caractes_nome = len(nome)
+print(f'O nome:{nome} possui {quant_caractes_nome} de letras')
