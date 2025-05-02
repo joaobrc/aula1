8740513f8e506cb0c18d7ccacbca1cb5f7afe75c
Joao <joaobrc97@gmail.com>
2025-05-02 13:42:25 -0300

    bug: Correção da soma nao estava sendo visualizada

--- src/aula1/exercicio_2.py
+++ src/aula1/exercicio_2.py
@@ -1,5 +1,5 @@
 #cria um programa onde o usuario digite dois valores e apareça a soma
-valor_1 = input('Relizando soma: \n digite o primeiro numero: ')
+valor_1 = input('Relizando soma: \n Digite o primeiro numero: ')
 valor_2 = input('Digite um segundo numero: ')
 soma = int(valor_1) + int(valor_2)
-print('Soma: {soma}\n Lembrando so realizar a soma de dois numeros')
+print(f'Soma: {soma}\n Lembrando so realizar a soma de dois numeros')
